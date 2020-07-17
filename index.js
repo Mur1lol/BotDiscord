a8e6eeaaba6e0684249e5297db7d4408f53bd30d
Murilo <bc.murilo.mbc@gmail.com>
2020-07-16 22:09:37 -0300

    Adicionando novo comando

--- index.js
+++ index.js
@@ -15,27 +15,33 @@ bot.on('message', msg => {
     if (mensagem === '!help') {
         const embed = {
             "description": bot.user.username + " é um bot criado para facilitar a divisão de grupos para a realização de partidas personalizadas."+
-            "\n\n Para começar, basta utilizar os comandos `!sortear` junto com a quantidade de equipes que você quer criar, e eu vou sortear todos os jogadores dentro do canal de voz em que  você está ou `!jogar` e depois escrever os nomes dos jogadores que irão participar :grin:\n",
+            "\n\nPara começar, basta utilizar os comandos `!sortear` junto com a quantidade de equipes que você quer criar, e eu vou sortear todos os jogadores dentro do canal de voz em que você está ou "+
+            "`!jogar` e digitar as posições em que os jogadores estão dentro do canal, ou também `!nomes` e depois escrever os nomes dos jogadores que irão participar :grin:\n",
             "color": 2943861,
             "author": {
                 "name": bot.user.username,
                 "icon_url": "https://cdn.discordapp.com/attachments/718710623344787528/719315215321137152/pizza.png"
             },
             "fields": [
-                {"name": "Exemplos", "value": "\n!jogar 2\n!sortear 3"},
-                { "name": "Observações", "value": "Por padrão o numero de times é 2, então se você usar apenas `!sortear` também vai funcionar.\nO número maximo de equipes é 4."}
+                {"name": "Exemplos", "value": "\n!sortear 2\n!jogar 3\n!nomes 4"},
+                { "name": "Observações", "value": "Por padrão o numero de equipes é 2, então se você usar apenas `!sortear` também vai funcionar."+
+                "\nO número maximo de equipes é 4."}
             ]
         };
         msg.channel.send({ embed });
     }
 
-    //DECLARAR OS JOGADORES
-    else if (mensagem === '!jogar') {
-        var numero_times = msg.content.replace("!jogar", ""); // Remove o "!jogar" e deixa apenas o numero de times
+    //SORTEAR JOGADORES POR MENSAGEM
+    else if (mensagem.startsWith("!nomes")) {
+        var numero_times = msg.content.replace("!nomes", ""); // Remove o "!nomes" e deixa apenas o numero de times
         var participantes = new Array();
         let x = 0;
 
-        msg.channel.send('Digite os nomes dos jogadores, os separando em virgulas (",").').then(() => {
+        embed = {
+            "description": "Digite os nomes dos jogadores, os separando em virgulas (',').",
+            "color": 1752220
+        };
+        msg.channel.send({ embed }).then(() => {
             const filter = m => msg.author.id === m.author.id;
 
             msg.channel.awaitMessages(filter, { time: 150000, max: 1, errors: ['time'] })
@@ -51,12 +57,14 @@ bot.on('message', msg => {
                         msg.channel.send({ embed });
                     }
                     else {
-                        msg.channel.send(':x: O numero de jogadores não é sificiente. (Número de Jogadores atuais: ' + participantes.length + ') :x:');
+                        mensagem_erro(participantes);
+                        msg.channel.send({ embed });
                     }
                     
                 })
                 .catch(() => {
-                    msg.channel.send('Ninguem vai jogar? :thinking:');
+                    mensagem_inativo();
+                    msg.channel.send({ embed });
                 });
         });
     }
@@ -66,21 +74,92 @@ bot.on('message', msg => {
         var numero_times = msg.content.replace("!sortear", ""); // Remove o "!sortear" e deixa apenas o numero de times
         var idvoice = msg.member.voice.channelID; // Identifica o canal de voz em que o usuario esta
         const voiceChannels = msg.guild.channels.cache.filter(c => c.id === idvoice && c.type === 'voice'); 
-        var nome = "";
-        var nomes, jogadores;
+        var jogadores, nome = "";
         
         for (const [id, voiceChannel] of voiceChannels) { 
             voiceChannel.members.forEach(member => nome += (member.displayName)+"#");
         }
-        nomes = nome.split("#");
-        jogadores = nomes.filter(empty);
+        jogadores = nome.split("#").filter(empty);
 
         if (sorteio(jogadores, numero_times)) {
             msg.channel.send({ embed });
         }
         else {
-            msg.channel.send(':x: O numero de jogadores não é sificiente. (Número de Jogadores atuais: ' + jogadores.length + ') :x:');
+            mensagem_erro(jogadores);
+            msg.channel.send({ embed });
         }
+    }
+
+    //SORTEAR JOGADORES ESPECIFICOS DENTRO DO CANAL DE VOZ
+    else if (mensagem.startsWith("!jogar")) {
+        var numero_times = msg.content.replace("!jogar", "");        
+        var idvoice = msg.member.voice.channelID;
+        const voiceChannels = msg.guild.channels.cache.filter(c => c.id === idvoice && c.type === 'voice');
+        
+        var participantes = new Array();
+        var nomes = new Array();
+        var jogadores, nome = "";
+        
+        embed = {
+            "description": "Digite a posição em que os jogadores estão dentro do canal, os separando em virgulas (',').",
+            "color": 1752220
+        };
+        msg.channel.send({ embed }).then(() => {
+            const filter = m => msg.author.id === m.author.id;
+
+            msg.channel.awaitMessages(filter, { time: 150000, max: 1, errors: ['time'] })
+                .then(messages => {
+                    resultado = messages.first().content.split(","); //Pegar o nome dos jogadores
+      
+                    for (const [id, voiceChannel] of voiceChannels) {
+                        voiceChannel.members.forEach(member => nome += (member.displayName) + "#");
+                    }
+                    jogadores = nome.split("#").filter(empty);
+
+                    for (let i = 0; i < resultado.length; i++) {
+                        posicao = parseInt(resultado[i])-1;
+                        if (resultado.length <= jogadores.length) {
+                            if (posicao >= 0 && posicao <= jogadores.length) {
+                                nomes[i] = jogadores[posicao];
+                            }
+                            else {
+                                embed = {
+                                    "color": 15158332,
+                                    "fields": [
+                                        { "name": "Erro", "value": "Posição inválida" }
+                                    ]
+                                };
+                                msg.channel.send({ embed });
+                                break;
+                            }
+                        }
+                        else {
+                            embed = {
+                                "color": 15158332,
+                                "fields": [
+                                    { "name": "Erro", "value": "Acho que tem gente demais pra esse sorteio :thinking:" }
+                                ]
+                            };
+                            msg.channel.send({ embed });
+                            break;
+                        }
+                    }
+
+                    if (nomes.length != 0) {
+                        if (sorteio(nomes, numero_times)) {
+                            msg.channel.send({ embed });
+                        }
+                        else {
+                            mensagem_erro(nomes)
+                            msg.channel.send({ embed });
+                        }
+                    }
+                })
+                .catch(() => {
+                    mensagem_inativo();
+                    msg.channel.send({ embed });
+                });
+        });
     }
 });
 
@@ -184,6 +263,22 @@ function sorteio(jogador, qtde) {
 
         return true;
     }
+}
+
+function mensagem_erro(jogador) {
+    embed = {
+        "color": 15158332,
+        "fields": [
+            { "name": "Erro", "value": "O numero de jogadores não é sificiente. (Número de Jogadores atuais: " + jogador.length + ")" }
+        ]
+    };
+}
+
+function mensagem_inativo() {
+    embed = {
+        "description": "Ninguem vai jogar? :thinking:",
+        "color": 15158332
+    };
 }
 
 bot.login(token);
