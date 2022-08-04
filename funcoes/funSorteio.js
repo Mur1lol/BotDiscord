6e128fbf0f68263f73abd4414db28f84bcabb3fc
Murilo <bc.murilo.mbc@gmail.com>
2022-08-04 15:41:18 -0300

    Atualizando função

--- funcoes/funSorteio.js
+++ funcoes/funSorteio.js
@@ -9,15 +9,6 @@ module.exports = {
         }
 
         return equipes;
-    },
-    sorteio: function (jogadores, maxJogadores, msg) {
-        var vencedores = [];
-        var jogador_embaralhado = embaralhar(jogadores);
-        var jogador_escolhido = escolherVencedores(jogador_embaralhado, maxJogadores);
-
-        vencedores.push({ "name": 'Vencedores', "value": jogador_escolhido[0], "inline": true });
-
-        return vencedores;
     }
 }
 
@@ -32,14 +23,6 @@ function embaralhar(jogadores) {
     }
 
     return time_padrao;
-}
-
-function escolherVencedores(time_padrao, maxJogadores) {
-    var jogadoresSorteados = [];
-
-    jogadoresSorteados.push(time_padrao.slice(0, maxJogadores));
-
-    return jogadoresSorteados;
 }
 
 function separarTimes(time_padrao, qtde) {
